b0854c7e082caad253b29eab2b297d5f980fab3f
Carlos <carlos@ingeniarius.pt>
2025-05-06 12:50:45 +0100

    Make the rviz files generic.

--- scripts/refactor_rviz_config.sh
+++ scripts/refactor_rviz_config.sh
@@ -2,6 +2,6 @@
 
 PACKAGE_PATH=$(rospack find mrs_openswarm_real)
 
-cp $PACKAGE_PATH/rviz/default.rviz /tmp/default.rviz
+cp $PACKAGE_PATH/tmux/just_flying/rviz/default.rviz /tmp/default.rviz
 
 sed -i "s/uav[0-9]/$UAV_NAME/g" /tmp/default.rviz
