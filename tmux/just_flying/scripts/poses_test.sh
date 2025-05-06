a07e1747174a07d047934b134ed78715ebd16ebf
Carlos <carlos@ingeniarius.pt>
2025-05-06 14:49:24 +0100

    Clean just flying workspace.

--- tmux/just_flying/scripts/poses_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rosservice call /$UAV_NAME/control_manager/goto "goal: [9.78, 45.25, 12.0, 0.0]"
-sleep 15
-rosservice call /$UAV_NAME/control_manager/goto "goal: [9.64, 54.55 , 12.0, 0.0]"
-sleep 15
-rosservice call /$UAV_NAME/control_manager/goto "goal: [2.00, 46.46, 12.0, 0.0]"
-sleep 15
