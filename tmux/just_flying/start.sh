a088b13d8683ca0e5111a11c35f32bb817265e02
Carlos <carlos@ingeniarius.pt>
2025-05-07 18:37:00 +0100

    Update start bash script to deal with iterator in the bag_file dir.

--- tmux/just_flying/start.sh
+++ tmux/just_flying/start.sh
@@ -9,6 +9,40 @@ cd "$SCRIPTPATH"
 export TMUX_SESSION_NAME=real_drone
 export TMUX_SOCKET_NAME=mrs
 
+# location for storing the bag files
+# * do not change unless you know what you are doing
+MAIN_DIR=~/"bag_files"
+
+# the project name
+# * is used to define folder name in ~/$MAIN_DIR
+export PROJECT_NAME="openswarm"
+
+# get the iterator
+ITERATOR_FILE="$MAIN_DIR/$PROJECT_NAME"/iterator.txt
+if [ -e "$ITERATOR_FILE" ]
+then
+  ITERATOR=`cat "$ITERATOR_FILE"`
+  ITERATOR=$(($ITERATOR+1))
+else
+  echo "iterator.txt does not exist, creating it"
+  mkdir -p "$MAIN_DIR/$PROJECT_NAME"
+  touch "$ITERATOR_FILE"
+  ITERATOR="1"
+fi
+echo "$ITERATOR" > "$ITERATOR_FILE"
+
+# create file for logging terminals' output
+LOG_DIR="$MAIN_DIR/$PROJECT_NAME/"
+SUFFIX=$(date +"%Y_%m_%d_%H_%M_%S")
+SUBLOG_DIR="$LOG_DIR/"$ITERATOR"_"$SUFFIX""
+TMUX_DIR="$SUBLOG_DIR/tmux"
+mkdir -p "$SUBLOG_DIR"
+mkdir -p "$TMUX_DIR"
+
+# link the "latest" folder to the recently created one
+rm "$LOG_DIR/latest" > /dev/null 2>&1
+ln -sf "$SUBLOG_DIR" "$LOG_DIR/latest"
+
 # start tmuxinator
 tmuxinator start -p ./session.yml
 
