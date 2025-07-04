29f554e186f9d01597480af7592ce28015389150
Carlos <carlos@ingeniarius.pt>
2025-07-04 16:28:25 +0100

    Update configuration for three drones.

--- tmux/just_flying/scripts/env_ferox.sh
+++ tmux/just_flying/scripts/env_ferox.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 ############## CHANGE #############
-export UAV_NAMES="[uav13]"
-# export UAV_NAMES="[uav6, uav7, uav8]"
+# export UAV_NAMES="[uav13]"
+export UAV_NAMES="[uav12, uav13, uav14]"
 # export UAV_NAMES="[uav6, uav7, uav8, uav9, uav10, uav11]"
 # export UAV_NAMES="[uav6, uav7, uav8, uav9, uav10, uav11, uav12, uav13, uav14]"
-export WORLD_NAME=ingeniarius	# forest, ingeniarius
+export WORLD_NAME=ingeniarius	# forest, ingeniarius, espoo
 # export WORLD_NAME=forest	# forest, ingeniarius
 export RESET_COMMAND_FLAG=0
 export COMPUTER_NAME="asus1"
-export SWARM_HEIGHT=6.0
+export SWARM_HEIGHT=6.0   # Height of swarm (m)
 
 ############## CONFIG #############
 # Get the hostname
@@ -28,12 +28,19 @@ export TOF_UP_PORT="/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0"
 export RESET_COMMAND="{broadcast: false, command: 246, confirmation: 0, param1: 1.0, param2: 0.0, param3: 0.0, param4: 0.0, param5: 0.0, param6: 0.0, param7: 0.0}"
 
 ##############  ROS  ##############
-# export ROS_MASTER_URI='http://192.168.8.170:11311'
-# export ROS_IP='192.168.8.170'
+CONTAINER_IP=$(hostname -I | awk '{print $1}')
+echo "Container IP is $CONTAINER_IP"
+
+# export ROS_MASTER_URI='http://192.168.0.178:11311'
+export ROS_IP='192.168.0.178'
+# export ROS_IP=$CONTAINER_IP
+# export ROS_HOSTNAME=$CONTAINER_IP
+export ROS_HOSTNAME='192.168.0.178'
 export ROSCONSOLE_FORMAT='[${severity}] [${node}] [${function}] [${line}]: ${message}'
 
 ############ MRS FILES ############
 export PLATFORM_CONFIG=./config/mrs_uav_system/$UAV_TYPE.yaml
+# export CUSTOM_CONFIG=./config/custom_config_gps.yaml
 export CUSTOM_CONFIG=./config/custom_config_garmin.yaml
 export NETWORK_CONFIG=./config/network_config.yaml
 
@@ -64,15 +71,15 @@ elif [ "$WORLD_NAME" = "forest" ]; then
 	export DATUM_LONGITUDE_START=-8.527359231666667
 	export DATUM_LATITUDE_END=41.216510440162686
 	export DATUM_LONGITUDE_END=-8.52715607577829
-# elif [ "$WORLD_NAME" = "espoo" ]; then
-# 	export WORLD_CONFIG=./config/worlds/world_local_forest.yaml
-#     export DATUM_LATITUDE=41.21711155
-#     export DATUM_LONGITUDE=-8.52746193
-#     export DATUM_HEADING=0.0
-# 	export DATUM_LATITUDE_START=41.21683395
-# 	export DATUM_LONGITUDE_START=-8.527359231666667
-# 	export DATUM_LATITUDE_END=41.216510440162686
-# 	export DATUM_LONGITUDE_END=-8.52715607577829
+elif [ "$WORLD_NAME" = "espoo" ]; then
+	export WORLD_CONFIG=./config/worlds/world_local_espoo.yaml
+    export DATUM_LATITUDE=41.21711155
+    export DATUM_LONGITUDE=-8.52746193
+    export DATUM_HEADING=0.0
+	export DATUM_LATITUDE_START=41.21683395
+	export DATUM_LONGITUDE_START=-8.527359231666667
+	export DATUM_LATITUDE_END=41.216510440162686
+	export DATUM_LONGITUDE_END=-8.52715607577829
 else
     echo "Unknown WORLD_NAME: $WORLD_NAME"
     exit 1
