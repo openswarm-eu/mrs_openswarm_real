97b211c0891c17c5eda3415f5c23b8980ded09fe
Carlos <carlos@ingeniarius.pt>
2025-07-11 18:18:13 +0100

    Adjust weight of uav and octo planner.

--- tmux/just_flying/scripts/env_ferox.sh
+++ tmux/just_flying/scripts/env_ferox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ############## CHANGE #############
-export UAV_NAMES="[uav9]"
+export UAV_NAMES="[uav15]"
 # export UAV_NAMES="[uav12, uav13, uav14]"
 # export UAV_NAMES="[uav6, uav7, uav8, uav9, uav10, uav11]"
 # export UAV_NAMES="[uav6, uav7, uav8, uav9, uav10, uav11, uav12, uav13, uav14]"
@@ -9,6 +9,7 @@ export WORLD_NAME=ingeniarius	# forest, ingeniarius, espoo
 # export WORLD_NAME=forest	# forest, ingeniarius
 export RESET_COMMAND_FLAG=0
 export COMPUTER_NAME="asus1"
+# export COMPUTER_NAME="Ikaros"
 export SWARM_HEIGHT=3.0   # Height of swarm (m)
 
 ############## CONFIG #############
