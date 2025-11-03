3b1ba4a005408165b861612be03ea2768d7ab840
Carlos <carlos@ingeniarius.pt>
2025-11-03 12:53:38 +0000

    Update octomap planeer config file.

--- tmux/forest/scripts/env.sh
+++ tmux/forest/scripts/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ############## CHANGE #############
-export UAV_NAMES="[uav9]"
+export UAV_NAMES="[uav9, uav10, uav11]"
 # export UAV_NAMES="[uav6, uav7, uav8, uav9, uav10, uav11, uav12, uav13, uav14, uav15]"
 export WORLD_NAME=forest	# forest, ingeniarius, alfena
 export RESET_COMMAND_FLAG=0
